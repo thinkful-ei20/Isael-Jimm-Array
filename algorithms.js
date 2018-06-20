3d4794974260e3393a0477d8b206655a49d23366
unknown <ajameslusk@gmail.com>
2018-06-20 15:07:48 -0400

    added filter

--- algorithms.js
+++ algorithms.js
@@ -11,22 +11,34 @@ function replaceSpaceLoop(str){
   let result;
 
   for(let i = 0; i < str.length; i++){
-   if(str.charAt(i) === ' ') {
+    if(str.charAt(i) === ' ') {
       let temp = str.slice(prevSpace,i) + '%20';
       firstHalf += temp;
       secondHalf = str.slice(i+1);
       prevSpace = i + 1;
       result = firstHalf  + secondHalf;
-   }
+    }
   }
   return result; 
 }
 
-console.log(replaceSpace('tauhida parveen'));
-console.log(replaceSpace('www.thinkful.com /tauh ida parv een'));
+// console.log(replaceSpace('tauhida parveen'));
+// console.log(replaceSpace('www.thinkful.com /tauh ida parv een'));
 
-console.log(replaceSpaceLoop('tauhida parveen'));
-console.log(replaceSpaceLoop('www.thinkful.com /tauh ida parv een'));
+// console.log(replaceSpaceLoop('tauhida parveen'));
+// console.log(replaceSpaceLoop('www.thinkful.com /tauh ida parv een'));
 
+const filterArray = (arr, filterNum) => {
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] < filterNum){
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
+};
 
+let testArray = [1,2,3,4,5,6,7,8,9];
+filterArray(testArray, 5);
+console.log(testArray);
 
